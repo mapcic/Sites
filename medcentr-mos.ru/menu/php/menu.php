6727d9068750edf4527be4431e16663694a38349
mapcic <mapcic@mail.ru>
2017-07-04 17:46:43 +0300

    Fix get menu's nodes

--- medcentr-mos.ru/menu/php/menu.php
+++ medcentr-mos.ru/menu/php/menu.php
@@ -1,64 +1,81 @@
 <?php
-$menus = array(
-	array(
-		'title' => '',
-		'ids' => '123, 234'
-	),
-	array(
-		'title' => '',
-		'ids' => '123'
-	)
-);
-
-$db = JFactory::getDbo();
-$query = $db->getQuery(true);
-
-function getChild($arr, $num, $id) {
+function getChild(&$arr, $num, $id) {
 	$out = array();
 	$key = array();
-	foreach ($arr[$num+1] as $key => $val) {
-		if ( $val[parent_id] == $id ) {
+
+	if ($num > count($arr)){
+		return $out;
+	}
+
+	foreach ($arr[$num] as $key => $val) {
+		if ( $val->parent_id == $id ) {
 			$out[] = $val;
 			$del[] = $key;
 		}
 	}
-	// add remove elements in array
+
+	foreach ($del as $key => $val) {
+		unset($arr[$num][$val]);
+	}
+
 	return $out;
 }
 
-function printChild($arr, $num, $id) {
-	foreach ($arr[$num] as $key => $val) {
-		$child = getChild($arr, $num, $id);
-		if (!empty($child)?) {
-			$out = $out.'<div>'.$val['title'].'</div>'.printChild();
-		} else {
-			$out = $out.'<div>'.$val['title'].'</div>';
-		}
+// function printChild(&$arr, $num, $id) {
+// 	foreach ($arr[$num] as $key => $val) {
+// 		$child = getChild($arr, $num, $id);
+// 		if (!empty($child)) {
+// 			$out = $out.'<div>'.$val->title.'</div>'.printChild($arr, $num, $id);
+// 		} else {
+// 			$out = $out.'<div>'.$val->title.'</div>';
+// 		}
+// 	}
+// 	return $out;	
+// }
+
+function printChild(&$arr, $num, $id) {
+	$out = '';
+	$child = getChild($arr, $num, $id);
+
+	if (empty($child)) {
+		return $out;	
 	}
-	return $out;	
+	
+	foreach ($child as $key => $val) {
+		$out = $out.'<div>'.$val->title.'</div>'.printChild($arr, $num+1, $val->id);
+	}
+
+	return $out;
 }
+
+$menus = array(
+	array(
+		'title' => 'Тест1',
+		'ids' => '351, 353'
+	)
+);
+
 
 $db = JFactory::getDbo();
 $out = array();
-
 foreach ($menus as $menu) {
 	// SELECT GROUP_CONCAT(CONCAT(`path`, '/.*') SEPARATOR '|') FROM `joomla_menu` WHERE `id` REGEXP '^351$|^353$'
 	$subQueryParent = $db->getQuery(true)
-		->select($db->qn('path'))
+		->select('GROUP_CONCAT(CONCAT('.$db->qn('path').', "/.*") SEPARATOR "|")')
 		->from($db->qn('#__menu'))
 		->where($db->qn('id').' REGEXP '.$db->q(str_replace(', ', '|', $menu['ids'])));
 
 	$subQueryFrom = $db->getQuery(true)
 		->select($db->qn(array('id', 'title', 'path', 'level', 'parent_id', 'link')))
 		->from($db->qn('#__menu'))
-		->where('('.$db->qn('id').'IN ['.$menu['ids'].'] OR '.$db->qn('path').'REGEXP ('.$subQueryParent.') AND'.$db->qn('published').' = 1');
+		->where($db->qn('id').' IN ('.$menu['ids'].') OR '.$db->qn('path').' REGEXP ('.$subQueryParent.') AND'.$db->qn('published').' = 1');
 
 	$query = $db->getQuery(true)
 		->select($db->qn(array('A.id', 'A.title', 'A.path', 'A.level', 'A.parent_id')))
 		->from('('.$subQueryFrom.') AS '.$db->qn('A'))
 		->join('left', 
 			$db->qn('#__content', 'B')
-			.'ON'.
+			.' ON '.
 			$db->qn('A.link').' LIKE CONCAT("index.php?option=com_content&view=article&id=",'.$db->qn('B.id').')')
 		->order($db->qn('A.title'));
 
@@ -71,21 +88,13 @@ foreach ($menus as $menu) {
 	}
 
 	$keys = array_keys($menuByLevel); 
-	sort($arrKeys); 
-	$levelOffset = $arrKeys[0];
+	sort($keys); 
+	$levelOffset = $keys[0];
 
-	foreach ($menuByLevel as $key => $val) {
-		$out = '<div class="level'.$key.'">'.$val['title'].'</div>'.printChild();
+	foreach ($menuByLevel[$levelOffset] as $key => $val) {
+		$out = '<div class="father">'.$val->title.'</div>'.printChild($menuByLevel, $levelOffset+1, $val->id);
 		echo $out;
-	}
 
-	// $list = empty($list = menuList($menusLevels, 1, $menuId))? array() : $list;
-	// $list = array_merge($menusLevels[$levelOffset], $list);
-	// array_walk($list, function(&$val, $key){
-	// 	foreach (array('id', 'parent_id') as $val) {
-	// 		unset($val->{$val});
-	// 	}
-	// });
-	// $outList = array_merge($outList, (empty($list)? array() : $list));
+		echo "Раз<br><br>";
+	}
 }
-// echo json_encode(array(submenu => $_POST['ids'], submenus => $outList));
