ce2de78044301b092cb16b5588c2a370e4c8cbf7
mapcic <mapcic@mail.ru>
2017-07-09 21:29:23 +0300

    MarketBox

--- medcentr-mos.ru/MarketBoxMachine/box.js
+++ medcentr-mos.ru/MarketBoxMachine/box.js
@@ -7,32 +7,37 @@ function initMarketBoxMachine() {
 				navNode = jQuery('<div class="circle"></div>').appendTo(bar)
 					.attr('ind', ind);
 		});
+		$this.find('.LinkssBoxMBM div').each(function(ind){
+			jQuery(this).attr('ind', ind);
+		});
+
+		$this.attr('num', $this.find('.ImgsBoxMBM div').length);
 
 		$this.find('.NavBarMBM div').attr('enable', 0).one('click', mbm_stopAnimate);
 		mbm_switchTo($this.find('.NavBarMBM div').first());
 		
-		var timer = setInterval(mbm_animate, 5000, ind);
+		var timer = setInterval(mbm_animate, 5000, $this);
 		$this.attr('animating', '1').attr('bw', 0).attr('timerMBM', timer);
 	});
 }
 
-function mbm_animate(ind) {
-	var $this = jQuery('.MarketBoxMachine[ind='+ind+']'),
+function mbm_animate(obj) {
+	var $this = obj,
 		num = $this.attr('num'),
 		bw = $this.attr('bw'),
-		enabled = $this.find('.NavBarMBM div[enabled=1]');
+		enabled = $this.find('.NavBarMBM div[enable=1]');
 	if ( bw == 1 ) {
-		if (0 > +enabled.attr('ind')) {
-			mbm_switchTo($this.find('.NavBarMBM div[ind='+(enabled.attr('ind')-1)+']'));
+		if (0 < +enabled.attr('ind')) {
+			mbm_switchTo(enabled.prev());
 		} else {
-			mbm_switchTo($this.find('.NavBarMBM div[ind='+(enabled.attr('ind')+1)+']'));
+			mbm_switchTo(enabled.next());
 			$this.attr('bw', 0);
 		}
 	} else {
-		if (num < +enabled.attr('ind')) {
-			mbm_switchTo($this.find('.NavBarMBM div[ind='+(enabled.attr('ind')+1)+']'));
+		if (num - 1 > +enabled.attr('ind')) {
+			mbm_switchTo(enabled.next());
 		} else {
-			mbm_switchTo($this.find('.NavBarMBM div[ind='+(enabled.attr('ind')-1)+']'));
+			mbm_switchTo(enabled.prev());
 			$this.attr('bw', 1);
 		}
 	}
@@ -47,13 +52,16 @@ function mbm_click(event) {
 
 function mbm_switchTo(obj) {
 	var MBM = obj.parents('.MarketBoxMachine'),
-		img = MBM.find('.ImgsBoxMBM div[ind="'+obj.attr('ind')+'"]');
+		img = MBM.find('.ImgsBoxMBM div[ind="'+obj.attr('ind')+'"]'),
+		link = MBM.find('.LinksBoxMBM div[ind="'+obj.attr('ind')+'"]');
 
 	MBM.find('.NavBarMBM div').attr('enable', 0);
 	obj.attr('enable', 1);
 	
 	MBM.find('.ImgBoxMBM')
-		.css('background-image', 'url('+img.text()+')');
+		.css('background-image', 'url('+img.text()+')')
+		.parents('a')
+			.attr('href', link.text());
 
 }
 
@@ -64,7 +72,8 @@ function mbm_stopAnimate(event) {
 		MBM = $this.parents('.MarketBoxMachine').attr('animating', 0);
 	
 	clearInterval(MBM.attr('timerMBM'));
-	$this.find('div').on('click', mbm_click);
+	$this.parents('.NavBarMBM').children().on('click', mbm_click);
+	mbm_switchTo($this);
 }
 
 jQuery(document).ready(function(){
