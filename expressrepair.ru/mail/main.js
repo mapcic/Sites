4e88c3156c4a24b00b565bdef4d450934d94881a
mapcic <mapcic@mail.ru>
2017-09-22 16:48:15 +0300

    Add form + acttion. Use jQueru jsSmtp.

--- expressrepair.ru/mail/main.js
+++ expressrepair.ru/mail/main.js
@@ -1,25 +1,27 @@
 function mail_init() {
-
+	jQuery('.mail .send').on('click', send_email);
 }
 
 function send_mail { 
 	var $this = jQuery(this),
 		form = $this.find('.mail'),
+		name = form.find('.name'),
+		phone = form.find('.phone'),
 		email = form.find('.email'),
 		msg = form.find('.msg');
 
-	if ( !is_email(email.val()) || !is_msg(msg.val)) {
-		return 0;
-	}
+	// if ( !is_email(email.val()) || !is_msg(msg.val)) {
+	// 	return 0;
+	// }
 
-Email.send(
-	"prmaximus@yandex.ru",
-	email.val(),
-	"Вопрос мастеру.",
-	'Имя:'+name.val()+'Телефон:'+phone.val()+'<br>'+msg.val(),
-);
+	Email.send(
+		"prmaximus@yandex.ru",
+		email.val(),
+		"Вопрос мастеру.",
+		'Имя:'+name.val()+'Телефон:'+phone.val()+'<br>'+msg.val(),
+		{token: "63cb3a19-2684-44fa-b76f-debf422d8b00"}
+	);
 }
-	// {token: "63cb3a19-2684-44fa-b76f-debf422d8b00"}
 
 function clear_string( str ) {
     return str.replace(/^\s+|\s+$/g,''); 
