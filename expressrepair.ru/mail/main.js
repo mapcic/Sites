09d59791f4404e18330231df2021622b1ff7f11e
mapcic <mapcic@mail.ru>
2017-09-22 16:32:52 +0300

    Add scipts.

---
+++ expressrepair.ru/mail/main.js
@@ -0,0 +1,38 @@
+function mail_init() {
+
+}
+
+function send_mail { 
+	var $this = jQuery(this),
+		form = $this.find('.mail'),
+		email = form.find('.email'),
+		msg = form.find('.msg');
+
+	if ( !is_email(email.val()) || !is_msg(msg.val)) {
+		return 0;
+	}
+
+Email.send(
+	"prmaximus@yandex.ru",
+	email.val(),
+	"Вопрос мастеру.",
+	'Имя:'+name.val()+'Телефон:'+phone.val()+'<br>'+msg.val(),
+);
+}
+	// {token: "63cb3a19-2684-44fa-b76f-debf422d8b00"}
+
+function clear_string( str ) {
+    return str.replace(/^\s+|\s+$/g,''); 
+}
+
+function is_email( email ) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function is_msg( msg ) {
+	if(clear_string(msg) == '') {
+		return false;
+	}
+	return true;
+}
